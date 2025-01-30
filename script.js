a37393ed302045bedc648bc3d09933b61e15043f
asde coeur <12coeur@gmail.com>
2025-01-30 10:34:03 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const trackButton = document.getElementById('trackButton');
     const clickCount = document.getElementById('clickCount');
     let count = 0;
+document.body.style.backgroundColor = "#27B793"; // Remplacez #f0f0f0 par la couleur de votre choix
 
     trackButton.addEventListener('click', () => {
         count++;
